a2c9d0649d2e136f70b7c9ca8d1034637a569995
ecesaruhan <148263584+ecesaruhan@users.noreply.github.com>
2023-11-29 04:19:50 +0300

    Odev3 tamamlandı.

--- Odev3/app.js
+++ Odev3/app.js
@@ -6,44 +6,24 @@ NOT: Kod her türlü n değeri için çalışmalıdır, sabit bir sayı değildi
 
 
 
+function asalSayilar(n) {
+    let asalSayilarDizisi = [];
+    let result = asalSayilarDizisi;
+    let i;
+    let k;
+    let count = 0;
+    for (i = 2; i <= n; i++) {
+        for (k = 2; k <= i; k++) {
+            if (i % k == 0) {
+                count++;
+            }
+        }
+        if (count == 1) {
+            result.push(i);
+        }
+        count = 0;
+    }
+    return asalSayilarDizisi;
+}
+console.log(asalSayilar(50));
 
-
-// function asalSayilar(n) {
-//     let asalSayilarDizisi = [];
-//     let i;
-//     let k;
-//     let count = 0;
-//     for (i = 1; i <= n; i++) {
-//         for (k = 1; k <= i; k++) {
-//             if (k % i == 0) {
-//                 count++;
-//                 if (count == 2) {
-//                     break;
-//                 }
-//             }
-//         }
-//         asalSayilarDizisi.push(i);
-//     }
-//     return asalSayilarDizisi;
-// }
-// console.log(asalSayilar(11));
-
-
-
-
-// function asalSayilar(n) {
-//     let asalSayilarDizisi = [];
-//     let i=2
-//     while (i <= n) {
-//         let k = 2;
-//             while (k < i) {
-//                 if (i % k != 0) {
-//                     k++;
-//                 }
-//             }
-//         asalSayilarDizisi.push(i);
-//         i++;
-//     }
-//     return asalSayilarDizisi;
-// }
-// console.log(asalSayilar(10);
